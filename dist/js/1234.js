b6abc80f1955fc23878ad1c7a3f617472fdd8e66
Nikita <nikitazapletin@gmail.com>
2022-02-24 15:05:41 +0300

    drag n drop
    DnD with sorting in one board

---
+++ dist/js/1234.js
@@ -0,0 +1,46 @@
+function dragNdrop () {
+    const listItems = document.querySelectorAll('.list__item');
+    const lists = document.querySelectorAll('.list');
+
+    for (let i = 0; i < listItems.length; i++) {
+        const item = listItems[i];
+
+        item.addEventListener('dragstart', () => {
+            draggedItem = item;
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 0);
+        });
+
+        item.addEventListener('dragend', () => {
+            setTimeout(() => {
+                item.style.display = 'block';
+                draggedItem = null;
+            }, 0);
+        });
+
+        item.addEventListener('dblclick', () => {
+            item.remove();
+        });
+
+        for (let j = 0; j < lists.length; j++) {
+            const list = lists[j];
+
+            list.addEventListener('dragover', e => e.preventDefault());
+
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = 'rgba(0,0,0,.3)';
+            });
+
+            list.addEventListener('dragleave', function(e) {
+                this.style.backgroundColor = 'rgba(0,0,0,0)';
+            });
+
+            list.addEventListener('drop', function(e) {
+                this.style.backgroundColor = 'rgba(0,0,0,0)';
+                this.append(draggedItem);
+            });
+        }
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ function boardIdent () {
     const boards = document.querySelectorAll('.boards__item');
     
     addTask(boards[boards.length - 1]);
+    // dragNdrop(boards[boards.length - 1]);
 
     const lists = boards[boards.length - 1].querySelector('.list');
 }
@@ -14,12 +15,7 @@ function boardIdent () {
 
 
 function addTask(board) {
-    
-    console.log(board);
-    
     const lists = board.querySelector('.list');
-    console.log(lists);
-
     const btn = board.querySelector('.add__btn');
     const addBtn = board.querySelector('.add__item-btn');
     const cancelBtn = board.querySelector('.cancel__item-btn');
@@ -56,10 +52,10 @@ function addTask(board) {
     }
 
     addBtn.addEventListener('click', () => {
-        const newItem = document.createElement('div');
+        const newItem = document.createElement('li');
         
         newItem.classList.add('list__item');
-        newItem.innerHTML = `<p> ${value} </p>`;
+        newItem.textContent = value;
         newItem.draggable = true;
         // newItem.textContent = value;
         lists.append(newItem);
@@ -77,12 +73,12 @@ boardIdent();
 
 function addBoard () {
     const boards = document.querySelector('.boards');
-    const board = document.createElement('div');
+    const board = document.createElement('ul');
     board.classList.add('boards__item');
     board.innerHTML = `
                 <span contenteditable="true" class="title">Введите название</span>
-                <div class="list">
-                </div>
+                <ul class="list">
+                </ul>
 
                 <div class="form">
                     <textarea class="textarea" placeholder="Введите название для этой карточки"></textarea>
@@ -95,8 +91,8 @@ function addBoard () {
                 </div>
 
                 <div class="add__btn"> <span>+</span> Добавить карточку</div>
+
     `;
-    // console.log(boards);
     boards.append(board);
 
     boardIdent();
@@ -117,50 +113,59 @@ changeTitle();
 let draggedItem = null;
 
 function dragNdrop () {
-    const listItems = document.querySelectorAll('.list__item');
-    const lists = document.querySelectorAll('.list');
+    const tasksListElement = document.querySelector(`.list`);
+    const taskElements = tasksListElement.querySelectorAll(`.list__item`);
 
-    for (let i = 0; i < listItems.length; i++) {
-        const item = listItems[i];
-
-        item.addEventListener('dragstart', () => {
-            draggedItem = item;
-            setTimeout(() => {
-                item.style.display = 'none';
-            }, 0);
-        });
-
-        item.addEventListener('dragend', () => {
-            setTimeout(() => {
-                item.style.display = 'block';
-                draggedItem = null;
-            }, 0);
-        });
-
-        item.addEventListener('dblclick', () => {
-            item.remove();
-        });
-
-        for (let j = 0; j < lists.length; j++) {
-            const list = lists[j];
-
-            list.addEventListener('dragover', e => e.preventDefault());
-
-            list.addEventListener('dragenter', function (e) {
-                e.preventDefault();
-                this.style.backgroundColor = 'rgba(0,0,0,.3)';
-            });
-
-            list.addEventListener('dragleave', function(e) {
-                this.style.backgroundColor = 'rgba(0,0,0,0)';
-            });
-
-            list.addEventListener('drop', function(e) {
-                this.style.backgroundColor = 'rgba(0,0,0,0)';
-                this.append(draggedItem);
-            });
-        }
+    for (const task of taskElements) {
+        task.draggable = true;
     }
+
+    tasksListElement.addEventListener(`dragstart`, (evt) => {
+     evt.target.classList.add(`selected`);
+    });
+
+    tasksListElement.addEventListener(`dragend`, (evt) => {
+     evt.target.classList.remove(`selected`);
+    });
+
+    const getNextElement = (cursorPosition, currentElement) => {
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
+
+    return nextElement;
+    };
+
+    tasksListElement.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
+
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains(`.list__item`);
+
+    console.log(isMoveable);
+
+    if (isMoveable) {
+        return;
+    }
+
+    const nextElement = getNextElement(evt.clientY, currentElement);
+
+    if (
+        nextElement && 
+        activeElement === nextElement.previousElementSibling ||
+        activeElement === nextElement
+    ) {
+        return;
+    }
+        
+    tasksListElement.insertBefore(activeElement, nextElement);
+    });
 }
 dragNdrop ();
 
